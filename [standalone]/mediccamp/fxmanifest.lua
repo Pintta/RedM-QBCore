c8ac6afa145ae093f03e67d0a31222dcc55062d5
Pintta <69728770+Pintta@users.noreply.github.com>
2022-12-17 09:05:05 +0200

    Delete [standalone] directory

--- [standalone]/mediccamp/fxmanifest.lua
+++
@@ -1,8 +0,0 @@
-fx_version 'cerulean'
-game 'rdr3'
-rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
-
-description 'Custom Medic camps'
-version '1.0.0'
-this_is_a_map 'yes'
-lua54 'yes'
